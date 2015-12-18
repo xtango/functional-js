a2832ddefbf7ad44a1673d2a30574d2b37307236
xtango <xtango@users.noreply.github.com>
2015-12-17 21:08:36 -0500

    Update isSorted.js

--- isSorted.js
+++ isSorted.js
@@ -1,7 +1,7 @@
 /** 
- * Recursive check if adjacent vals are sorted
+ * Recursive check if adjacent numbers are sorted
  */
-function isSorted(a) {
+function isSortedSimple(a) {
     function go(i) {
         if (i === 0) return true
         else if (a[i] < a[i - 1]) return false
@@ -11,4 +11,31 @@ function isSorted(a) {
     return go(a.length);
 }
 
-console.log(isSorted([1, 3, 0])); // false
+console.log(isSortedSimple([1, 2, 3])); // true
+console.log(isSortedSimple([1, 3, 0])); // false
+
+
+
+/**
+ * isSorted. Takes a lessThan comparison func.
+ */
+function isSorted(a, lessThan) {
+    function go(i) {
+        console.log('go: a[i]', a[i]);
+        if (i === 0) return true
+        else if (lessThan(a[i], a[i - 1])) return false
+        else return go(i - 1);
+    }
+
+    return go(a.length - 1);
+}
+
+var cities = [
+    { name: 'NYC', size: 20},
+    { name: 'LDN', size: 5 }
+];
+
+res = isSorted(cities, function (c1, c2) {
+    return c1.size <= c2.size
+});
+console.log(res); // -> false
