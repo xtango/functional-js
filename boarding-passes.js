437c4f962210eaa445ff70ba6a6fde53db7b937e
xtango <xtango@users.noreply.github.com>
2015-12-24 15:58:43 -0500

    Update boarding-passes.js

--- boarding-passes.js
+++ boarding-passes.js
@@ -1,8 +1,12 @@
 /**
- * You take a one-way indirect flight trip, not stopping twice at the same airport. 
- * You have 1 boarding pass for each leg of your trip, each containing source and destination airports.
- * All the boarding passes are randomly sorted.
- * Find the initial departure (very first src) and the final destination (last dst) airports.
+ * Given a set of randomly sorted boarding passes (Source Airport, Destination Airport), find 
+ * the initial departure (very first src) and the final destination (last dst) airports.
+ * 
+ * Assumptions: 
+ * - You take a one-way indirect flight trip, not stopping twice at the same airport. 
+ * - You have 1 boarding pass for each leg of your trip, each containing source and destination airports.
+ * 
+ * @example: The result should be:  {first: "YVR", last: "HKG"} for passes = [['JFK', 'ZRH'], ['BOM', 'HKG'], ['YVR', 'JFK'], ['ZRH', 'BOM']]
  */
 function findFirstAndLast(passes) {
     var srcs = passes.map(function(x) { return x[0]});
